6dcc1dff2ab1328100b5b31f4664b1262d04c3c4
Darianna-dev <dariannagallo@gmail.com>
2025-01-19 04:12:23 +0000

    se crean todas las variables

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 // let numeroSecreto = 6;
 let numeroSecreto1 = [1,2,3,4,5,6,7,8,9,10];
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
-let intentos = 1
-let numAleatorio = Math.random(numeroSecreto1) * numeroSecreto1.length
-let numeroSecreto= Math.ceil(numAleatorio)
+let numeroMaxPosible = 100;
+let numeroUsuario = 0;
+let intentos = 1;
+let maxIntentos = 5;
+let numAleatorio = Math.random(numeroSecreto1) * numeroMaxPosible;
+let numeroSecreto= Math.ceil(numAleatorio);
 
-console.log(numeroUsuario);
+// console.log(numeroUsuario);
 console.log(numAleatorio);
 console.log(numeroSecreto);
 
 
-while (intentos <= 5) {
+while (numeroUsuario !== numeroSecreto) {
 
-    if (numeroUsuario == numeroSecreto) {
-        alert(`Felicidades, Acertaste el numero. El número Secreto es el: ${numeroSecreto}`);
-        break;
-    } else if (numeroUsuario > numeroSecreto) {
-        alert('El numero secreto es menor que el que has introducido'); 
-        if (intentos == 5) {
-            alert(`Lo siento , has agotado los intentos, el número secreto era el: ${numeroSecreto}`);
-            break;
-        }
-        numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
-    } else {
-        alert('El numero secreto es mayor que el que has introducido');
-        if (intentos == 5) {
-            alert(`Lo siento , has agotado los intentos, el número secreto era el: ${numeroSecreto}`);
-            break;
-        }
-        numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
-    } 
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaxPosible} por favor:`));
 
-    intentos ++;
+        if (numeroUsuario === numeroSecreto) {
+            alert(`Felicidades, Acertaste el número Secreto ( ${numeroSecreto} ) y lo hiciste en ${intentos} ${intentos === 1 ? 'intento' : "intentos"}`);
+            break;
+        } else if (numeroUsuario > numeroSecreto) {
+            alert('El numero secreto es menor que el que has introducido'); 
+            } else {
+                alert('El numero secreto es mayor que el que has introducido');
+            } 
+                
+            intentos ++;
+        
+            if (intentos > maxIntentos ){
+                alert(`Lo siento, llegaste al máximo de los intentos ${intentos} y el número secreto era el: ${numeroSecreto}`);
+                break;
+            }
     
-
-
+    
 }
 
